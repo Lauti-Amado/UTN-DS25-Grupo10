274d314e8bc5d78d13b0f9041c6701d5baa46214
jcruz2013c@gmail.com <jcruz2013c@gmail.com>
2025-06-23 12:16:13 -0300

    agrege carpeta frontend con react y bootstrap instalado

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+
+  return (
+    <>
+     <p>probando codigo</p>
+    </>
+  )
+}
+
+export default App
