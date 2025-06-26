7bd4631debbb88a921d8e0a77691804743276fca
jcruz2013c@gmail.com <jcruz2013c@gmail.com>
2025-06-26 11:44:45 -0300

    coloque tamaño minimo de pagina home postulante y modifique el borde del carousel de ofertas

--- frontend/src/componentes/carousel.jsx
+++ frontend/src/componentes/carousel.jsx
@@ -4,7 +4,15 @@ import OfertaCard from './ofertaCard';
 
 function OfertasCarousel() {
   return (
-    <Carousel style={{border:"1px solid black", padding:"10px", width:"800px"}} interval={null}>
+    <Carousel 
+      style={{border:"1px solid black",
+      borderRadius:"30px" , 
+      padding:"10px", 
+      width:"800px"
+      }} 
+      interval={null} 
+      indicators={false}
+    >
       <Carousel.Item >
           <div style={{display:"flex",}}>
             <OfertaCard titulo={'Oferta 1'} categoria={'programacion'} texto={'texto descriptivo del trabajo'}/>
