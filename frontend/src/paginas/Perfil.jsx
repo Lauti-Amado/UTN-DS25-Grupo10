98c7f12ff5f2cbbe476b515865b97fbb7eaf2860
Julian Figueira <julianfigueira485@gmail.com>
2025-07-03 23:29:50 -0300

    Agrego el codigo que le agregue al Perfil principal

--- frontend/src/paginas/Perfil.jsx
+++ frontend/src/paginas/Perfil.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ContenidoInfoPerfil from '../componentes/ContenidoInfoPerfil';
+import PerfilesSugeridos from '../componentes/sugerenciasperfiles';
+import styles from './perfil.module.css';
 
 export default function Perfil() {
    
     return (
-        <div>
+        <article className={styles.article}>
            <ContenidoInfoPerfil />
-        </div>
+           <PerfilesSugeridos/>
+        </article>
     );
 }
