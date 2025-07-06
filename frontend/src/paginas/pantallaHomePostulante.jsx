1fd149b78abf8e3d497c3ea798f9f2dba91bcf7b
jcruz2013c@gmail.com <jcruz2013c@gmail.com>
2025-07-05 23:19:59 -0300

    agregue formulario cuando se toca el boton de postular en las empresas destacadas y acomode el scroll-box de trabajos y el recuadro de empresas destacadas

--- frontend/src/paginas/pantallaHomePostulante.jsx
+++ frontend/src/paginas/pantallaHomePostulante.jsx
@@ -29,10 +29,15 @@ export default  function PantallaHomePostulante () {
             ) : usuarioLogueado.rol === 'postulante' ? (
               
               <div>
-                <h2>Postulante</h2>
+                <h2 style={{textDecoration:" brown 1.5px underline"}}>Postulante</h2>
+                  
                   <OfertasCarousel />
                   <br></br>
-                  <CuadroEmpDest />
+                  
+                  <TrabajosDisponibles />
+                  < CuadroEmpDest/>
+                  
+                  
               </div>
             ) : (
               
@@ -50,7 +55,7 @@ export default  function PantallaHomePostulante () {
           
         
           
-          <TrabajosDisponibles />
+         
         
       </div>
     )
