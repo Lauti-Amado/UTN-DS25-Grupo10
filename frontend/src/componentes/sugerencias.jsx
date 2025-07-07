eea405519f2374aa2f86d14c505136c1a856f757
Yamil Tundis <yamiltundis6@gmail.com>
2025-07-06 22:18:59 -0300

    añadi un alert al pinchar en 'Ir al perfil' de otro usuario

--- frontend/src/componentes/sugerencias.jsx
+++ frontend/src/componentes/sugerencias.jsx
@@ -2,11 +2,15 @@ import imagen from '../assets/perfilx.png';
 import styles from '../paginas/perfil.module.css';
 
 function Sugerencias({ nombre, imagen }) {
+
+  function mostrarAlerta() {
+    alert(`Redirigiéndose al perfil de ${nombre} ...`)
+  }
   return (
     <div className={styles.unperfil}>
       <h2>{nombre}</h2>
       <div className={styles.perfil}>
-      <button className={styles.btnVerForm}>Ir al perfil</button>
+      <button className={styles.btnVerForm} onClick={mostrarAlerta}>Ir al perfil</button>
       <img className={styles.imagenPerfil} src={imagen} alt="imgperfil" /></div>
     </div>
   );
