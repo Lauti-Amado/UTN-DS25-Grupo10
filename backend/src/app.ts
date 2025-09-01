af7c7d16e35cbf75d533f04de4e3dd77912d049d
Julian Figueira <julianfigueira485@gmail.com>
2025-09-01 19:57:23 -0300

    Importo cors y dotenv a la app backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,24 @@
 import express from 'express';
 import { ofertaRoutes } from './routes/oferta.routes';
+import cors from 'cors';
+import dotenv from  'dotenv';
 import { usuarioRoutes } from './routes/usuario.routes';
 import { proyectoRouters } from './routes/proyecto.routes';
 import { logRequest } from './middlewares/logger.middleware';
 import { handleError } from './middlewares/error.middleware';
 
+dotenv.config();
+
 const app = express();
 const PORT = 3000;
 
+const corsOptions={
+  origin:process.env.FRONTEND_URL || 'http://localhost:5173/',
+  credentials:true
+};
+
 // Middleware para JSON
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Middleware log
