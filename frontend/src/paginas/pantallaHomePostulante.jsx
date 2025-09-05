295491b78af9b74d4423985b13622e7661da4e14
Julian Figueira <julianfigueira485@gmail.com>
2025-09-05 15:34:42 -0300

    Agrego comentarios

--- frontend/src/paginas/pantallaHomePostulante.jsx
+++ frontend/src/paginas/pantallaHomePostulante.jsx
@@ -1,5 +1,5 @@
-import React, { Component, useContext,useState } from 'react'
-import OfertasCarousel from '../componentes/carousel'
+import React, { useContext, useState } from 'react';
+import OfertasCarousel from '../componentes/carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DatosContexto } from '../datosContext';
 import CuadroEmpDest from '../componentes/cuadroEmpDest';
@@ -7,60 +7,47 @@ import TrabajosDisponibles from '../componentes/TrabajosDisponibles';
 import PostulantesScrollBox from '../componentes/PostulantesScrollBox';
 import { Button } from 'react-bootstrap';
 
-export default  function PantallaHomePostulante () {
+export default function PantallaHomePostulante() {
+  const { usuarioLogueado } = useContext(DatosContexto); // guardo el usuario logueado
+  const [mostrarEmpresas, setMostrarEmpresas] = useState(false);
 
-    const {usuarioLogueado} = useContext(DatosContexto) //guardo el usuario logueado
-    const [mostrarEmpresas,setMostrarEmpresas] = useState(false);
-    
-   
-    return (
-      <div className="vistaEstirada">
-        <div style={{textAlign:"start"}}>
-          {usuarioLogueado ? (
-            usuarioLogueado.rol === 'empleador' ? (
-              <div>
-       
-                <h2 style={{textDecoration:"brown 1.5px underline"}}>Empleador</h2>
-                <OfertasCarousel />
-                  <br></br>
-                <PostulantesScrollBox />
-              </div>
-            ) : usuarioLogueado.rol === 'postulante' ? (
-              
-              <div>
-                <h2 style={{textDecoration:" brown 1.5px underline"}}>Postulante</h2>
-                  
-                  <OfertasCarousel />
-                  <br></br>
+  return (
+    <div className="vistaEstirada">
+      <div style={{ textAlign: "start" }}>
+        {usuarioLogueado ? (
+          !usuarioLogueado.rolPostulante ? (
+            // Empleador
+            <div>
+              <h2 style={{ textDecoration: "brown 1.5px underline" }}>Empleador</h2>
+              <OfertasCarousel />
+              <br />
+              <PostulantesScrollBox />
+            </div>
+          ) : (
+            // Postulante
+            <div>
+              <h2 style={{ textDecoration: "brown 1.5px underline" }}>Postulante</h2>
+              <OfertasCarousel />
+              <br />
 
-                  
-                  <Button 
-                   variant='dark'
-                   className='mb-3 mt-3 float-end'
-                   
-                   onClick={() => setMostrarEmpresas (!mostrarEmpresas)}
-                   >
-                    {mostrarEmpresas ? 'Ocultar empresas destacadas' : 'Mostrar empresas destacadas'}
-                   </Button>
+              <Button
+                variant="dark"
+                className="mb-3 mt-3 float-end"
+                onClick={() => setMostrarEmpresas(!mostrarEmpresas)}
+              >
+                {mostrarEmpresas
+                  ? 'Ocultar empresas destacadas'
+                  : 'Mostrar empresas destacadas'}
+              </Button>
 
-                  <TrabajosDisponibles />
-
-                  {mostrarEmpresas && <CuadroEmpDest />}
-
-
-              </div>
-            ) : (
-              
-              <div>  
-                <h2>Administrador</h2>
-              </div>
-            )
-          ) : (<p>no has iniciado sesion</p>)
-          }
-
-          
-        </div>
+              <TrabajosDisponibles />
+              {mostrarEmpresas && <CuadroEmpDest />}
+            </div>
+          )
+        ) : (
+          <p>No has iniciado sesión</p>
+        )}
       </div>
-    )
-
+    </div>
+  );
 }
