e1cc9ae8716e6e47bb2338747482e9c36c797dbd
Lautaro Amado <lautaa256@gmail.com>
2025-09-13 00:57:33 -0300

    Agrego lo necesario para aplicar jwt al backend, solo falta adaptarlo al front

--- backend/src/routes/usuario.routes.ts
+++ backend/src/routes/usuario.routes.ts
@@ -1,33 +1,21 @@
-import { Router } from 'express';
-import * as usuarioController from '../controllers/usuario.controller';
-import { createProyectoSchema, updateProyectoSchema } from '../validations/proyecto.validation';
-import {validate} from '../middlewares/validation.middleware';
-import { createUsuarioSchema, updateUsuarioSchema } from '../validations/usuario.validations';
+import { Router } from "express";
+import * as usuarioController from "../controllers/usuario.controller";
+import { validate } from "../middlewares/validation.middleware";
+import { createUsuarioSchema, updateUsuarioSchema } from "../validations/usuario.validations";
+import { authenticate, authorize } from "../middlewares/auth.middleware";
 
 const router = Router();
 
-// Ruta para obtener todos los usuarios
-router.get('/', usuarioController.getAllUsuarios);
+router.post("/login", usuarioController.loginUsuarioController);
 
-// Ruta para obtener un usuario por ID
-router.get('/:id', usuarioController.getUsuarioById);
+// Rutas protegidas
+router.get("/", authenticate, authorize("ADMIN"), usuarioController.getAllUsuarios);
+router.get("/:id", authenticate, authorize("ADMIN"), usuarioController.getUsuarioById);
+router.post("/", validate(createUsuarioSchema), usuarioController.createUsuario);
+router.put("/:id", validate(updateUsuarioSchema), usuarioController.updateUsuario);
+router.delete("/:id", usuarioController.deleteUsuario);
 
-// Ruta para crear un usuario
-router.post('/', validate(createUsuarioSchema),usuarioController.createUsuario);
+router.get("/particulares/p", authenticate, authorize("ADMIN"), usuarioController.getAllUsuariosPostulantes);
+router.get("/particulares/e", authenticate, authorize("ADMIN"), usuarioController.getAllUsuariosEmpleadores);
 
-// Ruta para actualizar un usuario existente
-router.put('/:id', validate(updateUsuarioSchema), usuarioController.updateUsuario);
-
-// Ruta para eliminar un usuario
-router.delete('/:id', usuarioController.deleteUsuario);
-
-router.post('/login', usuarioController.loginUsuarioController);
-
-
-// Ruta para obtener todos los usuarios
-router.get('/particulares/p', usuarioController.getAllUsuariosPostulantes);
-
-router.get('/particulares/e', usuarioController.getAllUsuariosEmpleadores);
-
-// Exportamos las rutas como "usuarioRoutes"
 export const usuarioRoutes = router;
