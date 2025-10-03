171e1934190582d90787686c95bb11cf671059f9
LeonelPiquet <piquetleonel@gmail.com>
2025-10-03 16:33:03 -0300

    hice la conexión del front del formulario con el back, conectó bien, pero hay un error al guardar el formulario en la BD

--- backend/src/routes/formulario.routes.ts
+++ backend/src/routes/formulario.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import * as formularioController from '../controllers/formulario.controller';
 import { createFormularioSchema } from '../validations/formulario.validation';
 import { validate } from '../middlewares/validation.middleware';
+import { upload } from '../middlewares/upload.middleware';
 
 const router = Router()
 
@@ -11,5 +12,8 @@ router.get('/:id', formularioController.getFormulariosByOferta);
 //Ruta para crear un formulario
 router.post('/:id', validate(createFormularioSchema), formularioController.createFormulario);
 
+router.post('/:id', upload.single('curriculum'),validate(createFormularioSchema), formularioController.createFormulario);
+
 // con "/:id" me refiero al id de la oferta 
 export const formularioRoutes = router;
+
